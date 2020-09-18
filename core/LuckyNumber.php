3a74228588eda4cb09db087e38bc8089e2786a35
Dmitry Likhachov <dmitry_l@pula.market>
2020-09-18 08:40:46 +0300

    v1.3

--- core/LuckyNumber.php
+++ core/LuckyNumber.php
@@ -8,8 +8,62 @@ namespace Core;
  */
 class LuckyNumber
 {
-
     const ITERATION_BORDER = 3;
+
+    /**
+     * Getting a lucky ticket number
+     * @param array $getData
+     * @return mixed
+     */
+    public function gettingLuckyNumber(array $getData)
+    {
+        $start = microtime(true);
+        $listArray = [];
+        if (is_array($getData) && !empty($getData) && array_key_exists('first', $getData) && array_key_exists('end', $getData)) {
+            $firstNumber = $getData['first'];
+            $EndNumber = $getData['end'];
+            if (count($this->getListOfNumber($firstNumber)) <= 6 || count($this->getListOfNumber($EndNumber))  <= 6) {
+                if ($firstNumber > $EndNumber && ($firstNumber - $EndNumber) > 100) {
+                    for ($numberI = $EndNumber; $numberI <= $firstNumber; $numberI++) {
+                        if ($response = $this->getCheckOnIdentity($numberI))
+                            $listArray[] = $response;
+                    }
+                } elseif ($firstNumber < $EndNumber && ($EndNumber - $firstNumber) > 100) {
+                    for ($numberI = $firstNumber; $numberI <= $EndNumber; $numberI++) {
+                        if ($response = $this->getCheckOnIdentity($numberI))
+                            $listArray[] = $response;
+                    }
+                } else {
+                    throw new \Exception('The difference between the range of the first and the end must be at least 100.');
+                }
+                $time = microtime(true) - $start;
+                array_push($listArray, ['time' => 'Затраченное время(млс): ' . $time]);
+                return $listArray;
+            }
+            throw new \Exception('The range of numbers must be from 000001 to 999999!');
+        }
+        throw new \Exception('Array processing error!');
+    }
+    
+    /**
+     * Get the list of numbers.
+     * @param integer $number
+     * @return array
+     */
+    private function getListOfNumber(int $number): array
+    {
+        return str_split(strval($number));
+    }
+
+    /**
+     * Get count of split
+     * @param array $splitNumber
+     * @return integer
+     */
+    private function getCountSplit(array $splitNumber): int
+    {
+        return count($splitNumber) > self::ITERATION_BORDER ? self::ITERATION_BORDER : count($splitNumber);;
+    }
 
     /**
      * Get the number of the last three numbers. Recurse.
@@ -18,10 +72,9 @@ class LuckyNumber
      */
     private function getIntEndThree(int $number): int
     {
-        $snumber = strval($number);
-        $splitNumber = str_split($snumber);
+        $splitNumber = $this->getListOfNumber($number);
         $splitNumber = count($splitNumber) < self::ITERATION_BORDER ? $splitNumber : array_slice($splitNumber, count($splitNumber) - self::ITERATION_BORDER, count($splitNumber));
-        $countSplit = count($splitNumber) > self::ITERATION_BORDER ? self::ITERATION_BORDER : count($splitNumber);
+        $countSplit = $this->getCountSplit($splitNumber);
         $iCountOfSlice = count(array_slice($splitNumber, 0, $countSplit));
         $iCountOfSlice = $countSplit == 1 ? 1 : $iCountOfSlice;
         $responseNumber = 0;
@@ -38,14 +91,13 @@ class LuckyNumber
      */
     private function getIntFirstThree(int $number): int
     {
-        $snumber = strval($number);
-        $splitNumber = str_split($snumber);
-        $countSplit = count($splitNumber) > self::ITERATION_BORDER ? self::ITERATION_BORDER : count($splitNumber);
-        $iCountOfSlice = count(array_slice(str_split($snumber), 0, $countSplit));
+        $splitNumber = $this->getListOfNumber($number);
+        $countSplit = $this->getCountSplit($splitNumber);
+        $iCountOfSlice = count(array_slice($splitNumber, 0, $countSplit));
         $iCountOfSlice = $countSplit == 1 ? 1 : $iCountOfSlice;
         $responseNumber = 0;
         for ($i = 0; $i < $iCountOfSlice; $i++) {
-            $responseNumber += $snumber[$i];
+            $responseNumber += $splitNumber[$i];
         }
         return $responseNumber > 9 ? $this->getIntEndThree($responseNumber) : $responseNumber;
     }
@@ -57,39 +109,11 @@ class LuckyNumber
      */
     private function getCheckOnIdentity(int $number)
     {
-        return $this->getIntFirstThree($number) == $this->getIntEndThree($number) ? $number : false;
-    }
-
-    /**
-     * Getting a lucky ticket number
-     * @param array $getData
-     * @return mixed
-     */
-    public function gettingLuckyNumber(array $getData)
-    {
-        $start = microtime(true);
-        $listArray = [];
-        if (is_array($getData) && !empty($getData) && array_key_exists('first', $getData) && array_key_exists('end', $getData)) {
-            $firstNumber = $getData['first'];
-            $EndNumber = $getData['end'];
-            if ($firstNumber > $EndNumber && ($firstNumber - $EndNumber) > 100) {
-                for ($numberI = $EndNumber; $numberI <= $firstNumber; $numberI++) {
-                    if ($response = $this->getCheckOnIdentity($numberI))
-                        $listArray[] = $response;
-                }
-            } elseif ($firstNumber < $EndNumber && ($EndNumber - $firstNumber) > 100) {
-                for ($numberI = $firstNumber; $numberI <= $EndNumber; $numberI++) {
-                    if ($response = $this->getCheckOnIdentity($numberI))
-                        $listArray[] = $response;
-                }
-            } else {
-                throw new \Exception('The difference between the range of the first and the end must be at least 100.');
-            }
-            $time = microtime(true) - $start;
-            array_push($listArray, ['time' => 'Затраченное время(млс): ' . $time]);
-            return $listArray;
+        if(count($this->getListOfNumber($number)) == 6) {
+            return $this->getIntFirstThree($number) == $this->getIntEndThree($number) ? $number : false;
         }
-        throw new \Exception('Array processing error!');
-    }
+        return false;
 
+    }
+    
 }
